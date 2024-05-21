44cf9d13cf47be62cea4461e4097d034f41284fd
shixiongfei <xiongfei.shi@icloud.com>
2024-05-21 20:50:27 +0800

    Compile ta-lib

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+/*
+ * index.d.ts
+ *
+ * Copyright (c) 2024 Xiongfei Shi
+ *
+ * Author: Xiongfei Shi <xiongfei.shi(a)icloud.com>
+ * License: Apache-2.0
+ *
+ * https://github.com/shixiongfei/napi-talib
+ */
