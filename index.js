9bc5de86f12dbd870528e50debcb418dc4cc8648
shixiongfei <xiongfei.shi@icloud.com>
2024-06-07 22:46:27 +0800

    Add gencodes.js script

--- index.js
+++ index.js
@@ -10,11 +10,12 @@
  */
 
 const types = require("./types.js");
+const functions = require("./functions.js");
 
 try {
   const binding = require("./build/Release/napi_talib.node");
-  module.exports = Object.assign(binding, types);
+  module.exports = Object.assign(binding, Object.assign(types, functions));
 } catch {
   const binding = require("./build/Debug/napi_talib.node");
-  module.exports = Object.assign(binding, types);
+  module.exports = Object.assign(binding, Object.assign(types, functions));
 }
